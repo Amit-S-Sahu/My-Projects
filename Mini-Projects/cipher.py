1e575b4b5794a2825d12710b3b0cc8e0db8de982
Amit00199 <Amit.123854@stu.upes.ac.in>
2024-02-23 15:52:06 +0530

    Added codes

--- Mini-Projects/cipher.py
+++ Mini-Projects/cipher.py
@@ -5,15 +5,12 @@ def vigenere(message, key, direction=1):
 
     for char in message.lower():
 
-        # Append any non-letter character to the message
         if not char.isalpha():
             final_message += char
         else:        
-            # Find the right key character to encode/decode
             key_char = key[key_index % len(key)]
             key_index += 1
 
-            # Define the offset and the encrypted/decrypted letter
             offset = alphabet.index(key_char)
             index = alphabet.find(char)
             new_index = (index + offset*direction) % len(alphabet)
